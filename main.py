ea1bb562532abf81a9bad1498eee199c4b1b22e8
My Name <myEmail@example.com>
2022-11-19 00:03:56 +0500

    The last commit

--- main.py
+++ main.py
@@ -2,19 +2,40 @@ import sys
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtGui import QPainter, QColor
+from random import randint
 
 
-class MainWindow(QMainWindow):
+class Window(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        uic.loadUi('UI.ui')
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        self.paint()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            qp.setBrush(QColor('yellow'))
+            for j in range(randint(1, 10)):
+                n = randint(1, 300)
+                qp.drawEllipse(n, n, n, n)
+            qp.end()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = QMainWindow()
+    ex = Window()
     ex.show()
     sys.exit(app.exec())
